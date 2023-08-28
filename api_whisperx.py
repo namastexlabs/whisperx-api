001c1b9c404b27a388500e00c84114505ca1442b
Cezar Vasconcelos <97035956+vasconceloscezar@users.noreply.github.com>
2023-08-28 03:13:22 -0300

    feat(api): Add environment variables and authentication

--- api_whisperx.py
+++ api_whisperx.py
@@ -1,31 +1,41 @@
 import os
+import dotenv
+dotenv.load_dotenv()
 import subprocess
 import requests
 from fastapi import FastAPI, Depends, HTTPException, Query, Form, UploadFile
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import HTTPBearer, OAuth2PasswordBearer, HTTPAuthorizationCredentials
 from fastapi.responses import JSONResponse
 import sqlite3
 import logging
 import jwt
 
+
 # Initialize FastAPI app and configure logging
 app = FastAPI()
 logging.basicConfig(level=logging.INFO)
 
-# SQLite database initialization
-conn = sqlite3.connect("users.db")
-cursor = conn.cursor()
-cursor.execute(
-    "CREATE TABLE IF NOT EXISTS users (username TEXT PRIMARY KEY, password TEXT)"
-)
-conn.commit()
+def get_db():
+    conn = sqlite3.connect("users.db")
+    cursor = conn.cursor()
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS users (username TEXT PRIMARY KEY, password TEXT)"
+    )
+    conn.commit()
+    return conn, cursor
 
 # Secret key for JWT and master key for user creation
-SECRET_KEY = "namastex_secret_key"
-MASTER_KEY = "@Umasenha@2016"
+SECRET_KEY = os.environ.get("SECRET_KEY", 'secret_key')
+MASTER_KEY = os.environ.get("MASTER_KEY", 'master_key')
+HF_TOKEN = os.environ.get("HUGGING_FACE_TOKEN")
+API_PORT = os.environ.get("API_PORT", 11300)
+
+# Create an instance of HTTPBearer
+security = HTTPBearer()
+
 
 # OAuth2 token location
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth", auto_error=False)
 
 # JWT functions
 def create_jwt_token(data: dict):
@@ -40,28 +50,39 @@ def decode_jwt_token(token: str):
         raise HTTPException(status_code=401, detail="Invalid token")
 
 # Dependency for authentication
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    return decode_jwt_token(token)
 
-@app.post("/token")
+async def get_current_user(authorization: HTTPAuthorizationCredentials = Depends(security)):
+    token = authorization.credentials
+    try:
+        payload = decode_jwt_token(token)
+        return payload
+    except:
+        raise HTTPException(status_code=403, detail="Forbidden")
+    
+
+@app.post("/auth")
 def get_token(username: str, password: str):
+    conn, cursor = get_db()
     cursor.execute("SELECT password FROM users WHERE username = ?", (username,))
     row = cursor.fetchone()
+    conn.close()
     if row and row[0] == password:
         return {"access_token": create_jwt_token({"sub": username}), "token_type": "bearer"}
     else:
         raise HTTPException(status_code=401, detail="Invalid username or password")
 
-@app.post("/register_user")
+@app.post("/create_user")
 def register_user(username: str, password: str, master_key: str = Query(...)):
     if master_key != MASTER_KEY:
         return JSONResponse(status_code=403, content={"detail": "Not authorized"})
-    
+    conn, cursor = get_db()
     try:
         cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
         conn.commit()
+        conn.close()
         return {"detail": "User created successfully"}
     except sqlite3.IntegrityError:
+        conn.close()
         return JSONResponse(status_code=400, content={"detail": "Username already exists"})
 
 @app.post("/whisperex-transcribe/")
@@ -69,11 +90,10 @@ async def process_video(
     file: UploadFile = None,
     min_speakers: int = Form(1),
     max_speakers: int = Form(2),
-    current_user: dict = Depends(get_current_user)  # Add this line
+    current_user: dict = Depends(get_current_user)  
 ):
     try:
         logging.info("Starting video processing.")
-
         # Create the directory if it doesn't exist
         if not os.path.exists('./temp'):
             os.makedirs('./temp')
@@ -87,22 +107,33 @@ async def process_video(
         
         logging.info("File saved successfully.")
 
-        # Step 2: Convert the video to MP3 using ffmpeg
-        temp_mp3_path = temp_video_path.replace(".mp4", ".mp3")
-        subprocess.run(["ffmpeg", "-i", temp_video_path, temp_mp3_path], check=True)
-        logging.info("Converted video to MP3.")
+        # Step 2: Convert the video or audio to MP3 using ffmpeg
+        file_extension = os.path.splitext(file.filename)[-1].lower()
+        supported_formats = [".mp4", ".avi", ".mkv", ".flv", ".mov", ".wmv", ".webm", ".mp3", ".wav", ".flac"]
+        
+        if file_extension not in supported_formats:
+            logging.error("Unsupported file format.")
+            raise HTTPException(status_code=415, detail="Unsupported Media Type")
 
+        temp_mp3_path = os.path.splitext(temp_video_path)[0] + ".mp3"
+        subprocess.run(["ffmpeg", "-y", "-i", temp_video_path, temp_mp3_path], check=True)
+        logging.info("Converted video/audio to MP3.")
         # Step 3: Run the whisperx command on the MP3 file
-        output_dir = "./data/namastex/"
-        cmd = f"whisperx {temp_mp3_path} --model large-v2 --language pt --hf_token  --output_format all --output_dir {output_dir}  --align_model WAV2VEC2_ASR_LARGE_LV60K_960H --diarize --min_speakers {min_speakers} --max_speakers {max_speakers}"
+        output_dir = "./data/"
+        cmd = f"whisperx {temp_mp3_path} --model large-v2 --language pt --hf_token {HF_TOKEN} --output_format all --output_dir {output_dir}  --align_model WAV2VEC2_ASR_LARGE_LV60K_960H --diarize --min_speakers {min_speakers} --max_speakers {max_speakers}"
         subprocess.run(cmd.split())
         logging.info("Ran whisperx command.")
 
         # Step 4: Read the .vtt file
         vtt_path = [f for f in os.listdir(output_dir) if f.endswith(".vtt")][0]
-        with open(os.path.join(output_dir, vtt_path), "r") as vtt_file:
-            vtt_content = vtt_file.read()
+        with open(os.path.join(output_dir, vtt_path), "r") as txt_file:
+            vtt_content = txt_file.read()
         logging.info("Read .vtt file.")
+        
+         # Step 5: Read the .TXT file
+        txt_path = [f for f in os.listdir(output_dir) if f.endswith(".txt")][0]
+        with open(os.path.join(output_dir, txt_path), "r") as txt_file:
+            txt_content = txt_file.read()
 
         # Clean up temporary files
         os.remove(temp_video_path)
@@ -110,7 +141,7 @@ async def process_video(
         logging.info("Cleaned up temporary files.")
 
         logging.info("Video processing completed successfully.")
-        return {"status": "success", "vtt_content": "your_vtt_content", "summary": "your_summary"}
+        return {"status": "success", "vtt_content":vtt_content , "text_content": txt_content}
 
     except Exception as e:
         logging.error(f"An error occurred: {str(e)}")
@@ -118,4 +149,4 @@ async def process_video(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=11300)
+    uvicorn.run(app, host="0.0.0.0", port=int(API_PORT))
