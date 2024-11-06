9cc5c667f6c583d849b8b0967c25ebc5cd319b20
odrec <renatogarita@gmail.com>
2024-11-06 14:26:28 +0100

    Fix bug when filename has blank spaces

--- src/utils/transcription_utils.py
+++ src/utils/transcription_utils.py
@@ -1,4 +1,5 @@
 import subprocess
+import shlex
 
 from src.api.config import HF_TOKEN
 
@@ -6,10 +7,10 @@ from src.api.config import HF_TOKEN
 def run_whisperx(temp_mp3_path, lang, model, min_speakers, max_speakers):
     output_dir = "./data/"
     if min_speakers > 0 and max_speakers > 0:
-        cmd = (f"whisperx {temp_mp3_path} --model {model} --language {lang} --hf_token {HF_TOKEN} --output_format all "
+        cmd = (f"whisperx {shlex.quote(temp_mp3_path)} --model {model} --language {lang} --hf_token {HF_TOKEN} --output_format all "
                f"--output_dir {output_dir}  --align_model WAV2VEC2_ASR_LARGE_LV60K_960H --diarize --min_sp"
                f"eakers {min_speakers} --max_speakers {max_speakers}")
     else:
-        cmd = (f"whisperx {temp_mp3_path} --model {model} --language {lang} --output_format all "
+        cmd = (f"whisperx {shlex.quote(temp_mp3_path)} --model {model} --language {lang} --output_format all "
                f"--output_dir {output_dir}  --align_model WAV2VEC2_ASR_LARGE_LV60K_960H")
-    subprocess.run(cmd.split(), check=True)
+    subprocess.run(shlex.split(cmd), check=True)
