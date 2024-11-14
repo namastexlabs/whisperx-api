2aca16ef2a978492e70731a7b4f7d6e0549872ff
odrec <renatogarita@gmail.com>
2024-11-14 13:33:45 +0100

    Cast min and max speakers as string instead of using int values for the command.

--- src/utils/transcription_utils.py
+++ src/utils/transcription_utils.py
@@ -14,8 +14,8 @@ def run_whisperx(temp_mp3_path, lang, model, min_speakers, max_speakers):
                "--output_dir", output_dir,
                "--align_model", "WAV2VEC2_ASR_LARGE_LV60K_960H",
                "--diarize",
-               "--min_speakers", min_speakers,
-               "--max_speakers", max_speakers]
+               "--min_speakers", str(min_speakers),
+               "--max_speakers", str(max_speakers)]
     else:
         cmd = ["whisperx", temp_mp3_path,
                "--model", model,
