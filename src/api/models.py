ed7f3e13ed2baf75279f4f5be7575d4a97e00b11
odrec <renatogarita@gmail.com>
2025-01-28 13:14:43 +0100

    Keep only the languages that have ASR alignment models on whisperx.

--- src/api/models.py
+++ src/api/models.py
@@ -2,13 +2,8 @@ from enum import Enum
 
 
 class LanguageEnum(str, Enum):
-    af = "af"  # Afrikaans
     ar = "ar"  # Arabic
-    hy = "hy"  # Armenian
-    az = "az"  # Azerbaijani
-    be = "be"  # Belarusian
-    bs = "bs"  # Bosnian
-    bg = "bg"  # Bulgarian
+    eu = "eu"  # Basque
     ca = "ca"  # Catalan
     zh = "zh"  # Chinese
     hr = "hr"  # Croatian
@@ -16,49 +11,35 @@ class LanguageEnum(str, Enum):
     da = "da"  # Danish
     nl = "nl"  # Dutch
     en = "en"  # English
-    et = "et"  # Estonian
     fi = "fi"  # Finnish
     fr = "fr"  # French
     gl = "gl"  # Galician
+    ka = "ka"  # Georgian
     de = "de"  # German
     el = "el"  # Greek
     he = "he"  # Hebrew
     hi = "hi"  # Hindi
     hu = "hu"  # Hungarian
-    is_ = "is"  # Icelandic
-    id = "id"  # Indonesian
     it = "it"  # Italian
     ja = "ja"  # Japanese
-    kn = "kn"  # Kannada
-    kk = "kk"  # Kazakh
     ko = "ko"  # Korean
     lv = "lv"  # Latvian
-    lt = "lt"  # Lithuanian
-    mk = "mk"  # Macedonian
-    ms = "ms"  # Malay
-    mr = "mr"  # Marathi
-    mi = "mi"  # Maori
-    ne = "ne"  # Nepali
-    no = "no"  # Norwegian
+    ml = "ml"  # Malayalam
+    no = "no"  # Norwegian (Bokmål)
+    nn = "nn"  # Norwegian (Nynorsk)
     fa = "fa"  # Persian
     pl = "pl"  # Polish
     pt = "pt"  # Portuguese
     ro = "ro"  # Romanian
     ru = "ru"  # Russian
-    sr = "sr"  # Serbian
     sk = "sk"  # Slovak
     sl = "sl"  # Slovenian
     es = "es"  # Spanish
-    sw = "sw"  # Swahili
-    sv = "sv"  # Swedish
-    tl = "tl"  # Tagalog
-    ta = "ta"  # Tamil
-    th = "th"  # Thai
+    te = "te"  # Telugu
     tr = "tr"  # Turkish
     uk = "uk"  # Ukrainian
     ur = "ur"  # Urdu
     vi = "vi"  # Vietnamese
-    cy = "cy"  # Welsh
 
 
 class ModelEnum(str, Enum):
